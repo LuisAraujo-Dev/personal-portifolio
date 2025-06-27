efa9314e18059f21309f5019c585348fef1edb14
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-06-26 22:15:32 -0300

    add img LA marca on footer

--- scripts/menu.js
+++ scripts/menu.js
@@ -34,4 +34,18 @@ document.addEventListener("DOMContentLoaded", function () {
     el.classList.add("sectionHidden");
     observer.observe(el);
   });
+
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 });
